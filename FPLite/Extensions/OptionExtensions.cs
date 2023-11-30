f23c34bd4885d647ec64c5b1b456fcf40cf8b40b
Paulo Miranda <paulojdcmiranda@gmail.com>
2023-11-30 09:57:47 +0100

    Added extension method for casting to an Option type

--- FPLite/Extensions/OptionExtensions.cs
+++ FPLite/Extensions/OptionExtensions.cs
@@ -12,4 +12,17 @@ public static class OptionExtensions
     /// <returns>An <see cref="Option{T}"/> instance containing the associated value if value is not null
     /// or an instance representing no value.</returns>
     public static Option<T> ToOption<T>(this T? value) => value is null ? Option<T>.None : Option<T>.Some(value);
+
+    /// <summary>
+    /// Converts a nullable value of type <typeparamref name="TIn"/> to an <see cref="Option{TOut}"/>.
+    /// </summary>
+    /// <typeparam name="TIn">The input type, a nullable type.</typeparam>
+    /// <typeparam name="TOut">The output type, a reference type.</typeparam>
+    /// <param name="value">The nullable value to be converted.</param>
+    /// <returns>
+    /// An <see cref="Option{TOut}"/> containing the non-null casted value if it is of type <typeparamref name="TOut"/>,
+    /// or an empty <see cref="Option{TOut}"/> if the value is null or not of the expected type.
+    /// </returns>
+    public static Option<TOut> AsOptionOf<TIn, TOut>(this TIn? value) where TOut : class =>
+        value is TOut cast ? Option<TOut>.Some(cast) : Option<TOut>.None;
 }
