0a1a01f32521e21055aa426609e499ddb57c8938
Paulo Miranda <paulojdcmiranda@gmail.com>
2024-02-27 10:01:34 +0100

    Simplified extensions, created extensions for new Result type, added compatibility with Net Standard 2.1

--- FPLite.Extensions/EitherExtensions.cs
+++ FPLite.Extensions/EitherExtensions.cs
@@ -1,18 +1,17 @@
-﻿namespace FPLite.Extensions;
-
-public static class EitherExtensions
+﻿namespace FPLite.Extensions
 {
-    /// <summary>
-    /// Converts a nullable left value to an Either monad.
-    /// </summary>
-    /// <typeparam name="TLeft">The type of the left value.</typeparam>
-    /// <typeparam name="TRight">The type of the right value.</typeparam>
-    /// <param name="left">The nullable left value to convert.</param>
-    /// <returns>
-    /// Either monad representing the conversion result.
-    /// If the provided left value is null, returns a representation of "neither" value.
-    /// Otherwise, returns a representation of the left value.
-    /// </returns>
-    public static Either<TLeft, TRight> ToEither<TLeft, TRight>(this TLeft? left) =>
-        left is null ? Either<TLeft, TRight>.Neither : Either<TLeft, TRight>.Left(left);
+    public static class EitherExtensions
+    {
+        /// <summary>
+        /// Converts value to an Either monad with a left value.
+        /// </summary>
+        public static Either<TLeft, TRight> ToEither<TLeft, TRight>(this TLeft left) =>
+            Either<TLeft, TRight>.Left(left);
+
+        /// <summary>
+        /// Converts value to an Either monad with a right value.
+        /// </summary>
+        public static Either<TLeft, TRight> ToEither<TLeft, TRight>(this TRight right) =>
+            Either<TLeft, TRight>.Right(right);
+    }
 }
--- FPLite.Extensions/UnionExtensions.cs
+++ FPLite.Extensions/UnionExtensions.cs
@@ -1,90 +1,49 @@
 ﻿using FPLite.Union;
 
-namespace FPLite.Extensions;
-
-public static class UnionExtensions
+namespace FPLite.Extensions
 {
-    /// <summary>
-    /// Converts a nullable value of type T1 to a Union representing 2 possible types.
-    /// </summary>
-    /// <param name="t1">The nullable value of type T1 to convert.</param>
-    /// <returns>
-    /// Union representing 2 possible types.
-    /// If the provided value is null, returns a representation of "nothing" value.
-    /// Otherwise, returns a representation of the value as T1.
-    /// </returns>
-    public static Union<T1, T2> ToUnion<T1, T2>(this T1? t1) =>
-        t1 is null ? Union<T1, T2>.Nothing : Union<T1, T2>.Type1(t1);
+    public static class UnionExtensions
+    {
+        /// <summary>
+        /// Converts a nullable value of type T1 to a Union representing 2 possible types.
+        /// </summary>
+        public static Union<T1, T2> ToUnion<T1, T2>(this T1 t1) =>
+            Union<T1, T2>.Type1(t1);
 
-    /// <summary>
-    /// Converts a nullable value of type T1 to a Union representing 3 possible types.
-    /// </summary>
-    /// <param name="t1">The nullable value of type T1 to convert.</param>
-    /// <returns>
-    /// Union representing 3 possible types.
-    /// If the provided value is null, returns a representation of "nothing" value.
-    /// Otherwise, returns a representation of the value as T1.
-    /// </returns>
-    public static Union<T1, T2, T3> ToUnion<T1, T2, T3>(this T1? t1) =>
-        t1 is null ? Union<T1, T2, T3>.Nothing : Union<T1, T2, T3>.Type1(t1);
+        /// <summary>
+        /// Converts a nullable value of type T1 to a Union representing 3 possible types.
+        /// </summary>
+        public static Union<T1, T2, T3> ToUnion<T1, T2, T3>(this T1 t1) =>
+            Union<T1, T2, T3>.Type1(t1);
 
-    /// <summary>
-    /// Converts a nullable value of type T1 to a Union representing 4 possible types.
-    /// </summary>
-    /// <param name="t1">The nullable value of type T1 to convert.</param>
-    /// <returns>
-    /// Union representing 4 possible types.
-    /// If the provided value is null, returns a representation of "nothing" value.
-    /// Otherwise, returns a representation of the value as T1.
-    /// </returns>
-    public static Union<T1, T2, T3, T4> ToUnion<T1, T2, T3, T4>(this T1? t1) =>
-        t1 is null ? Union<T1, T2, T3, T4>.Nothing : Union<T1, T2, T3, T4>.Type1(t1);
+        /// <summary>
+        /// Converts a nullable value of type T1 to a Union representing 4 possible types.
+        /// </summary>
+        public static Union<T1, T2, T3, T4> ToUnion<T1, T2, T3, T4>(this T1 t1) =>
+            Union<T1, T2, T3, T4>.Type1(t1);
 
-    /// <summary>
-    /// Converts a nullable value of type T1 to a Union representing 5 possible types.
-    /// </summary>
-    /// <param name="t1">The nullable value of type T1 to convert.</param>
-    /// <returns>
-    /// Union representing 5 possible types.
-    /// If the provided value is null, returns a representation of "nothing" value.
-    /// Otherwise, returns a representation of the value as T1.
-    /// </returns>
-    public static Union<T1, T2, T3, T4, T5> ToUnion<T1, T2, T3, T4, T5>(this T1? t1) =>
-        t1 is null ? Union<T1, T2, T3, T4, T5>.Nothing : Union<T1, T2, T3, T4, T5>.Type1(t1);
+        /// <summary>
+        /// Converts a nullable value of type T1 to a Union representing 5 possible types.
+        /// </summary>
+        public static Union<T1, T2, T3, T4, T5> ToUnion<T1, T2, T3, T4, T5>(this T1 t1) =>
+            Union<T1, T2, T3, T4, T5>.Type1(t1);
 
-    /// <summary>
-    /// Converts a nullable value of type T1 to a Union representing 6 possible types.
-    /// </summary>
-    /// <param name="t1">The nullable value of type T1 to convert.</param>
-    /// <returns>
-    /// Union representing 6 possible types.
-    /// If the provided value is null, returns a representation of "nothing" value.
-    /// Otherwise, returns a representation of the value as T1.
-    /// </returns>
-    public static Union<T1, T2, T3, T4, T5, T6> ToUnion<T1, T2, T3, T4, T5, T6>(this T1? t1) =>
-        t1 is null ? Union<T1, T2, T3, T4, T5, T6>.Nothing : Union<T1, T2, T3, T4, T5, T6>.Type1(t1);
+        /// <summary>
+        /// Converts a nullable value of type T1 to a Union representing 6 possible types.
+        /// </summary>
+        public static Union<T1, T2, T3, T4, T5, T6> ToUnion<T1, T2, T3, T4, T5, T6>(this T1 t1) =>
+            Union<T1, T2, T3, T4, T5, T6>.Type1(t1);
 
-    /// <summary>
-    /// Converts a nullable value of type T1 to a Union representing 7 possible types.
-    /// </summary>
-    /// <param name="t1">The nullable value of type T1 to convert.</param>
-    /// <returns>
-    /// Union representing 7 possible types.
-    /// If the provided value is null, returns a representation of "nothing" value.
-    /// Otherwise, returns a representation of the value as T1.
-    /// </returns>
-    public static Union<T1, T2, T3, T4, T5, T6, T7> ToUnion<T1, T2, T3, T4, T5, T6, T7>(this T1? t1) =>
-        t1 is null ? Union<T1, T2, T3, T4, T5, T6, T7>.Nothing : Union<T1, T2, T3, T4, T5, T6, T7>.Type1(t1);
+        /// <summary>
+        /// Converts a nullable value of type T1 to a Union representing 7 possible types.
+        /// </summary>
+        public static Union<T1, T2, T3, T4, T5, T6, T7> ToUnion<T1, T2, T3, T4, T5, T6, T7>(this T1 t1) =>
+            Union<T1, T2, T3, T4, T5, T6, T7>.Type1(t1);
 
-    /// <summary>
-    /// Converts a nullable value of type T1 to a Union representing 8 possible types.
-    /// </summary>
-    /// <param name="t1">The nullable value of type T1 to convert.</param>
-    /// <returns>
-    /// Union representing 8 possible types.
-    /// If the provided value is null, returns a representation of "nothing" value.
-    /// Otherwise, returns a representation of the value as T1.
-    /// </returns>
-    public static Union<T1, T2, T3, T4, T5, T6, T7, T8> ToUnion<T1, T2, T3, T4, T5, T6, T7, T8>(this T1? t1) =>
-        t1 is null ? Union<T1, T2, T3, T4, T5, T6, T7, T8>.Nothing : Union<T1, T2, T3, T4, T5, T6, T7, T8>.Type1(t1);
+        /// <summary>
+        /// Converts a nullable value of type T1 to a Union representing 8 possible types.
+        /// </summary>
+        public static Union<T1, T2, T3, T4, T5, T6, T7, T8> ToUnion<T1, T2, T3, T4, T5, T6, T7, T8>(this T1 t1) =>
+            Union<T1, T2, T3, T4, T5, T6, T7, T8>.Type1(t1);
+    }
 }
